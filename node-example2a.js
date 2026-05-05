e706358705bff3872c69c510f96db99cee325710
Guri60 <samambarsarwala08@gmail.com>
2026-05-04 22:47:40 -0700

    initial commit
    initial commit

---
+++ node-example2a.js
@@ -0,0 +1,2 @@
+const circle = require('./node-example2.js');
+console.log('the area of the circle of radius 4 is ${circle.area(4)}');
---
+++ node-http/server.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  console.log(`Request for ${req.url} by method ${req.method}`);
+
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+
+  res.end('<html><body><h1>Hello World!</h1></body></html>');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
